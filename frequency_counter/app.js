50d83f4af2d8b4778b5831a86b3e379f16eac796
Carbonell <m.carbonell@trio-group.de>
2018-10-22 09:48:37 +0200

    finished d3

--- frequency_counter/app.js
+++ frequency_counter/app.js
@@ -1,3 +1,16 @@
+d3.select('#reset')
+    .on("click", () => {
+        d3.selectAll('.letter')
+            .remove();
+        
+        d3.select('#phrase')
+            .text('');
+
+        d3.select('#count')
+            .text("");
+    })
+
+
 d3.select('form') 
     .on('submit', () => {
         d3.select('.analysis').classed('noshow', false)
@@ -5,40 +18,49 @@ d3.select('form')
         let input = d3.select('input');
         let text = input.property('value');
 
-        d3.select('#letters')
-            .selectAll('.letter')
-            .data(counter(text))
+        const letters = d3.select('#letters')
+                            .selectAll('.letter')
+                            .data(counter(text), d => d.character)
             // all functions return the data passed here as the first param 
+            letters.classed("new", false)
+                .exit()
+                .remove()
+
+            letters
             .enter()
             .append('div')
                 .classed('letter', true)
+                .classed('new', true)
+            .merge(letters)
                 .style('width', '20px')
                 .style('line-height', '20px')
                 .style('height', d => {
-                    return d.counts * 20 + 'px';
+                    return d.count * 20 + 'px';
                 })
                 .text(d => {
-                    return d.characters;
+                    return d.character;
                 })
         
-        d3.select('analysis')
-            .select('span')
-                .text(text);
+        d3.select('#phrase')
+            .text("Analysis of: " + text);
+
+        d3.select('#count')
+            .text("New characters: " + letters.enter().nodes().length);
         
         input.property('value', '');
     })
 
 const counter = str => {
+    let sorted = str.split('').sort();
     let data = [];
-    str.split('').sort().reduce((acc, c) => {
-            acc[c] = (acc[c]++ || 0) + 1;
-            if(c != ' ') {
-                data.push({characters : c, counts: acc[c]});
-            }
-            return acc;
-    }, {})
+    for (let i = 0; i < sorted.length; i++) {
+        const element = sorted[i];
+        let last = data[data.length - 1];
+        last && last.character == sorted[i] ? last.count++ : data.push({ character: sorted[i], count: 1 });
+
+    }
     return data;
 }
 
-console.log(counter('marcos es maricooo'));
+console.log(counter('marcosesmaricooo'));
 
